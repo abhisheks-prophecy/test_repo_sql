dbb00f6fb391170ea101d68b81957ac6382b7466
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-11-11 17:09:08 +0000

    ❇️ New     - Pipeline        - SC_CONFIG_PIP

---
+++ testprojects/master_scala_mainone/pipelines/SC_CONFIG_PIP/code/src/main/scala/io/prophecy/pipelines/sc_config_pip/config/Config.scala
@@ -0,0 +1,36 @@
+package io.prophecy.pipelines.sc_config_pip.config
+
+import io.prophecy.pipelines.sc_config_pip.config.ConfigStore._
+import pureconfig._
+import io.prophecy.libs._
+
+case class Config(
+  c_string:  String = "asdaslnjd#!@#%^&*()=-asd",
+  c_int:     Int = -12313,
+  c_float:   Float = 21312.123f,
+  c_double:  Double = 9.07234123e8d,
+  c_boolean: Boolean = true,
+  c_short:   Short = 12,
+  c_databricks_secret: DatabricksSecret =
+    DatabricksSecret(scope = "qasecrets_mysql", key = "username"),
+  c_spark_expression: String = "concat(`c  - int`, `c-string`)"
+) extends ConfigBase
+
+object DatabricksSecret {
+
+  implicit val myIntReader: ConfigReader[DatabricksSecret] =
+    ConfigReader[String].map { s =>
+      val Array(scope, key) = s.split(":")
+      DatabricksSecret(scope, key)
+    }
+
+}
+
+case class DatabricksSecret(scope: String, key: String) {
+
+  override def toString: String = {
+    import com.databricks.dbutils_v1.DBUtilsHolder.dbutils
+    dbutils.secrets.get(scope = scope, key = key)
+  }
+
+}
