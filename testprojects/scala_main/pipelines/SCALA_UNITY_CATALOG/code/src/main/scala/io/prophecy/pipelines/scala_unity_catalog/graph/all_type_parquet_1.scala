bceee500b40866ab48d3b944d313e87ce048a923
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-11-23 09:19:24 +0000

    🔁 Modified     - Pipeline        - DONOT_OPEN_SCALA        - SCALA_SG_SRC        - LIVY_SCALA        - SCALA_DEP_MGMT_ALL        - SCALA_BASIC        - SCALA_AZURE     - Job        - DONOT_OPEN_SCALA_JOB        - REL_SC_CUSTOMLIBS        - REL_SC_DBFSLIBS        - SCALA_SINGLE_CLUSTER_JOB
    ❇️ New
        - Dataset
           - src_parquet_unity_catalog
           - qa_database_all_type_parquet
        - Pipeline
           - SCALA_UNITY_CATALOG

---
+++ testprojects/scala_main/pipelines/SCALA_UNITY_CATALOG/code/src/main/scala/io/prophecy/pipelines/scala_unity_catalog/graph/all_type_parquet_1.scala
@@ -0,0 +1,17 @@
+package io.prophecy.pipelines.scala_unity_catalog.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.scala_unity_catalog.config.ConfigStore._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object all_type_parquet_1 {
+
+  def apply(spark: SparkSession): DataFrame =
+    spark.read.table("qa_database.all_type_parquet")
+
+}
