0234e93ab12e012437697c36022957018e62e50c
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-11-24 17:40:33 +0000

    🔁 Modified     - Pipeline        - PYTHON_ACL_HC_SHARED        - PYTHON_BASIC        - PYTHON_AZURE        - PythonGenericUse        - PYTHON_SG_SRC        - PYTHON_UNITY_CATALOG        - PYTHON_DEP_MGMT_ALL        - LIVY_PYTHON     - Job        - REL_PY_CUSTOMLIBS        - PYTHON_SINGLE_CLUSTER_JOB        - DONOT_OPEN_PYTHON_JOB        - REL_PY_DBFSLIBS     - Subgraph        - pythonLivySG1

---
+++ testprojects/python_main/pipelines/LIVY_PYTHON/code/test/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/test_Reformat_6_1.py
@@ -0,0 +1,100 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from argparse import Namespace
+from prophecy.test import BaseTestCase
+from prophecy.test.utils import *
+from livy_python.graph.Subgraph_4.Subgraph_2_1.Subgraph_3_1.Reformat_6_1 import *
+import livy_python.config.ConfigStore as ConfigStore
+
+
+class Reformat_6_1Test(BaseTestCase):
+
+    def test_unit_test_0(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/in0/schema.json',
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/in0/data/test_unit_test_0.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/out/schema.json',
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/out/data/test_unit_test_0.json',
+            'out'
+        )
+        dfOutComputed = Reformat_6_1(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select(
+              "year",
+              "industry_code_ANZSIC",
+              "industry_name_ANZSIC",
+              "rme_size_grp",
+              "variable",
+              "value",
+              "unit"
+            ),
+            dfOutComputed.select(
+              "year",
+              "industry_code_ANZSIC",
+              "industry_name_ANZSIC",
+              "rme_size_grp",
+              "variable",
+              "value",
+              "unit"
+            ),
+            self.maxUnequalRowsToShow
+        )
+
+    def test_unit_test_1(self):
+        dfIn0 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/in0/schema.json',
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/in0/data/test_unit_test_1.json',
+            'in0'
+        )
+        dfOut = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/out/schema.json',
+            'test/resources/data/livy_python/graph/Subgraph_4/Subgraph_2_1/Subgraph_3_1/Reformat_6_1/out/data/test_unit_test_1.json',
+            'out'
+        )
+        dfOutComputed = Reformat_6_1(self.spark, dfIn0)
+        assertDFEquals(
+            dfOut.select(
+              "year",
+              "industry_code_ANZSIC",
+              "industry_name_ANZSIC",
+              "rme_size_grp",
+              "variable",
+              "value",
+              "unit"
+            ),
+            dfOutComputed.select(
+              "year",
+              "industry_code_ANZSIC",
+              "industry_name_ANZSIC",
+              "rme_size_grp",
+              "variable",
+              "value",
+              "unit"
+            ),
+            self.maxUnequalRowsToShow
+        )
+
+    def setUp(self):
+        BaseTestCase.setUp(self)
+        import os
+        fabricName = os.environ['FABRIC_NAME']
+        ConfigStore.Utils.initializeFromArgs(
+            self.spark,
+            Namespace(file = f"configs/resources/config/{fabricName}.json", config = None)
+        )
+        dfgraph_Lookup_1 = createDfFromResourceFiles(
+            self.spark,
+            'test/resources/data/livy_python/graph/Lookup_1/schema.json',
+            'test/resources/data/livy_python/graph/Lookup_1/data.json',
+            "in0"
+        )
+        from livy_python.graph.Lookup_1 import Lookup_1
+        Lookup_1(self.spark, dfgraph_Lookup_1)
--- testprojects/python_main/subgraphs/pythonLivySG1/code/org/python/livy/pythondepmanagement_1/graph/pythonLivySG1_1/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Filter_1_1 import *
-from .Subgraph_2_1 import *
-from .OrderBy_1_1 import *
-from .Reformat_4_1 import *
-from .pythonLivySG1_1 import pythonLivySG1_1
