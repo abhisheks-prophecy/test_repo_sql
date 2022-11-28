07d03ac0b27a8d3cf23d44e4f839d05687a20e3e
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-11-28 13:12:54 +0000

    Simple workflow Commit

---
+++ test/project_build_tool/scala/pipelines/Automated-scala-PBT-1669640932953/code/src/main/scala/io/prophecy/pipelines/automatedscalapbt1669640932953/udfs/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.automatedscalapbt1669640932953.udfs
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
