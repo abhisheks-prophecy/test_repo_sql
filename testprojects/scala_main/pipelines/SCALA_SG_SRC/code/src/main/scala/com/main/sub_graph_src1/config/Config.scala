0060d710ce2cbb732f2fb1fc1ec2b687145dc5f1
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-12-05 10:27:10 +0000

    changes

--- testprojects/scala_main/pipelines/SCALA_SG_SRC/code/src/main/scala/com/main/sub_graph_src1/config/Config.scala
+++ testprojects/scala_main/pipelines/SCALA_SG_SRC/code/src/main/scala/com/main/sub_graph_src1/config/Config.scala
@@ -2,5 +2,6 @@ package com.main.sub_graph_src1.config
 
 import com.main.sub_graph_src1.config.ConfigStore._
 import pureconfig._
+import pureconfig.generic.ProductHint
 import io.prophecy.libs._
 case class Config() extends ConfigBase
