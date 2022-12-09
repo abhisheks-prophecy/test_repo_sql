965e4904ab332bc76fcb0b5c3bb06a85bdbb4689
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-12-09 07:26:02 +0000

    🔁 Modified     - Subgraph        - all_type     - Pipeline        - SCALA_DEP_MGMT_ALL        - SCALA_SG_SRC     - Job        - SCALA_SINGLE_CLUSTER_JOB     - Dataset        - src_parquet_all_type_and_partition_withspacehyphens

---
+++ testprojects/scala_main/subgraphs/all_type/code/src/main/scala/org/scala/sub_graph_src1/graph/Subgraph_1/Script_1_1.scala
@@ -0,0 +1,20 @@
+package org.scala.sub_graph_src1.graph.Subgraph_1
+
+import io.prophecy.libs._
+import com.main.sub_graph_src1.config.ConfigStore._
+import com.main.sub_graph_src1.udfs.UDFs._
+import com.main.sub_graph_src1.udfs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Script_1_1 {
+  def apply(spark: SparkSession, in0: DataFrame): DataFrame = {
+    var out0=in0
+    out0
+  }
+
+}
