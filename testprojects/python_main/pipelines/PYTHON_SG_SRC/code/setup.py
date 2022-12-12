778081617be40e52a16a09b0a7f033cfabd6a14d
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-12-12 12:29:31 +0000

    🔁 Modified     - Pipeline        - PYTHON_GCP        - PYTHON_DEP_MGMT_ALL        - DONOT_OPEN_PYTHON        - PYTHON_SG_SRC        - PYTHON_ACL_HC_SHARED     - Dataset        - src_parquet_all_type_and_partition_withspacehyphens_renamed

--- testprojects/python_main/pipelines/PYTHON_SG_SRC/code/setup.py
+++ testprojects/python_main/pipelines/PYTHON_SG_SRC/code/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(
-    name = 'PYTHON_SG_SRC',
+    name = 'REL_PY_PIP_SG_SRC',
     version = '1.0',
     packages = find_packages(include = ('com.sg_src.main*', )) + ["prophecy_config_instances"],
     package_dir = {'prophecy_config_instances' : 'configs/resources/config'},
     package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
     description = 'workflow',
     install_requires = [
-'prophecy-libs==1.3.11'],
+'prophecy-libs==1.3.12'],
     entry_points = {
 'console_scripts' : [
 'main = com.sg_src.main.pipeline:main'], },
