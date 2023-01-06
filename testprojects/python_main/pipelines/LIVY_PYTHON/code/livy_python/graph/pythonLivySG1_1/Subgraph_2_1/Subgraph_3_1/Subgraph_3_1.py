5bd877c24ba2829ab4ce04648528858dbdb58fec
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-01-06 16:07:30 +0000

    🔁 Modified     - Pipeline        - PYTHON_BASIC        - PYTHON_DEP_MGMT_ALL        - LIVY_PYTHON        - PERF_PY_PIP     - Job        - REL_PY_DBFSLIBS        - PYTHON_SINGLE_CLUSTER_JOB

--- testprojects/python_main/pipelines/LIVY_PYTHON/code/livy_python/graph/pythonLivySG1_1/Subgraph_2_1/Subgraph_3_1/Subgraph_3_1.py
+++ testprojects/python_main/pipelines/LIVY_PYTHON/code/livy_python/graph/pythonLivySG1_1/Subgraph_2_1/Subgraph_3_1/Subgraph_3_1.py
@@ -6,5 +6,12 @@ from . import *
 
 def Subgraph_3_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
     df_Reformat_6_1 = Reformat_6_1(spark, in0)
+    df_Reformat_6_1 = collectMetrics(
+        spark, 
+        df_Reformat_6_1, 
+        "Subgraph_3_1", 
+        "1xLXn460ujreIMutMC-0m$$wRd6Me0MI-Kd3ZtMTc0U-", 
+        "kpOriftOdJJGBQ_slHfYM$$yH-cJp3tUilsZsoLuG8WI"
+    )
 
     return df_Reformat_6_1
