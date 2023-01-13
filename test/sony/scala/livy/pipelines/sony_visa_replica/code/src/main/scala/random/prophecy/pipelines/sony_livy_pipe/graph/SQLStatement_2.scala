5b6357a8552f6ac997353e05c98ed15416c0ceaf
sony@prophecy.io <sony@prophecy.io>
2023-01-13 09:44:55 +0000

    changes

---
+++ test/sony/scala/livy/pipelines/sony_visa_replica/code/src/main/scala/random/prophecy/pipelines/sony_livy_pipe/graph/SQLStatement_2.scala
@@ -0,0 +1,22 @@
+package random.prophecy.pipelines.sony_livy_pipe.graph
+
+import io.prophecy.libs._
+import random.prophecy.pipelines.sony_livy_pipe.config.ConfigStore._
+import random.prophecy.pipelines.sony_livy_pipe.config.Context
+import random.prophecy.pipelines.sony_livy_pipe.udfs.UDFs._
+import random.prophecy.pipelines.sony_livy_pipe.udfs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object SQLStatement_2 {
+
+  def apply(context: Context, in0: DataFrame): DataFrame = {
+    in0.createOrReplaceTempView("in0")
+    context.spark.sql("select * from in0")
+  }
+
+}
