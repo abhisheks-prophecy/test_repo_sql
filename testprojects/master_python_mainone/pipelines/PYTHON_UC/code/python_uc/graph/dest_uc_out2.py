f9c77394f0c2aded60753e538f277baa146b8020
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-02-22 07:20:41 +0000

    🔁 Modified     - Pipeline        - PYTHON_DEP_MANAGEMENT_ALL        - PythonDoAnything        - PYTHON_LIVY        - PYTHON_SUBGRAPH_CREATOR     - Job        - MASTER_PY_SC_DEPMGMT
    ❇️ New
        - Dataset
           - src_parquet_3part_uc
           - dest_uc_1
           - dest_uc_2
           - qa_database_acl_table_with_read_permission_for_user
           - qa_database_all_type_parquet
        - Pipeline
           - PYTHON_UNITY_CATALOG

---
+++ testprojects/master_python_mainone/pipelines/PYTHON_UC/code/python_uc/graph/dest_uc_out2.py
@@ -0,0 +1,9 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from python_uc.config.ConfigStore import *
+from python_uc.udfs.UDFs import *
+
+def dest_uc_out2(spark: SparkSession, in0: DataFrame):
+    in0.write.format("parquet").mode("overwrite").save("dbfs:/tmp/e2e/test/dest_uc_2")
