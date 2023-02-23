200d5328920b6dc3aea4a5aaa83db30948402949
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-02-23 07:34:23 +0000

    ❇️ New     - Subgraph        - SCALA_SG
    🔁 Modified
        - Pipeline
           - SCALA_PIPELINE

--- test/e2e/regressions/scala_project_donotopen/pipelines/SCALA_PIPELINE/code/src/main/scala/io/prophecy/pipelines/scala_pipeline/main.scala
+++ test/e2e/regressions/scala_project_donotopen/pipelines/SCALA_PIPELINE/code/src/main/scala/io/prophecy/pipelines/scala_pipeline/main.scala
@@ -7,6 +7,8 @@ import io.prophecy.pipelines.scala_pipeline.config._
 import io.prophecy.pipelines.scala_pipeline.udfs.UDFs._
 import io.prophecy.pipelines.scala_pipeline.udfs._
 import io.prophecy.pipelines.scala_pipeline.graph._
+import io.prophecy.pipelines.scala_pipeline.graph.Subgraph_1
+import io.prophecy.pipelines.scala_pipeline.graph.SCALA_SG
 import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
@@ -18,7 +20,9 @@ object Main {
 
   def apply(context: Context): Unit = {
     val df_dataset_cust_in = dataset_cust_in(context)
-    val df_Reformat_1      = Reformat_1(context, df_dataset_cust_in)
+    val df_Reformat_1      = Reformat_1(context,       df_dataset_cust_in)
+    val df_Subgraph_1      = Subgraph_1.apply(context, df_Reformat_1)
+    val df_SCALA_SG        = SCALA_SG.apply(context,   df_Reformat_1)
   }
 
   def main(args: Array[String]): Unit = {
