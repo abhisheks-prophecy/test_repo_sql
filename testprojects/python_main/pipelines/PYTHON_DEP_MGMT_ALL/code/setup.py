1f7356a3b0e2f30988dd7e6183787bf84e7277ea
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-03-28 07:06:18 +0000

    🔁 Modified     - Pipeline        - REL_PY_PIP_DEP_MGMT_ALL        - PYTHON_BASIC        - LIVY_PYTHON

--- testprojects/python_main/pipelines/PYTHON_DEP_MGMT_ALL/code/setup.py
+++ testprojects/python_main/pipelines/PYTHON_DEP_MGMT_ALL/code/setup.py
@@ -7,7 +7,7 @@ setup(
     package_data = {'prophecy_config_instances' : ['*.json', '*.py']},
     description = 'workflow',
     install_requires = ['numpy==1.22.*', 'pandas>=1.4.2', 'torch==1.11.0', 'matplotlib==3.5.2', 'scipy>=1.6.3,<=1.8.1', 'requests~=2.28.0',
-     'prophecy-libs==1.4.8'],
+     'prophecy-libs==1.4.7'],
     entry_points = {
 'console_scripts' : [
 'main = com.main1.pythondepmanagement_1.pipeline:main'], },
