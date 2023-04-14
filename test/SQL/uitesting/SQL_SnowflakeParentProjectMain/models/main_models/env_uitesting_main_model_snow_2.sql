9f7c68f390cbe9c901440e6bf00518b39c5171e0
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-04-14 07:46:01 +0000

    ❇️ New     - Source        - alias_base_QA_DATABASE_QA_SIMPLE_SCHEMA        - alias_base_QA_DATABASE_QA_SCHEMA     - Function        - qa_macro_call_another_macro_column        - qa_concat_macro_column     - Model        - env_uitesting_main_model_snow_3
    🔁 Modified
        - Function
           - qa_macro_call_another_macro
           - qa_boolean_macro
           - qa_concat_macro
        - Model
           - env_uitesting_main_model_snow_1
           - env_uitesting_main_model_snow_2
    
    ❌ Deleted
        - Source
           - QA_DATABASE.QA_SIMPLE_SCHEMA

--- test/SQL/uitesting/SQL_SnowflakeParentProjectMain/models/main_models/env_uitesting_main_model_snow_2.sql
+++ test/SQL/uitesting/SQL_SnowflakeParentProjectMain/models/main_models/env_uitesting_main_model_snow_2.sql
@@ -18,6 +18,14 @@ Reformat_1 AS (
 
 ),
 
+ALL_TYPE_TABLE_NON_GEOMETRY AS (
+
+  SELECT * 
+  
+  FROM {{ source('alias_base_QA_DATABASE_QA_SCHEMA', 'ALL_TYPE_TABLE_NON_GEOMETRY') }}
+
+),
+
 goods_classification AS (
 
   SELECT * 
@@ -37,11 +45,14 @@ Join_1 AS (
     in1.NZHSC_Level_1_Code_HS2 AS NZHSC_Level_1_Code_HS2,
     in1.NZHSC_Level_2 AS NZHSC_Level_2,
     in1.NZHSC_Level_1 AS NZHSC_Level_1,
-    in1.Status_HS4 AS Status_HS4
+    in1.Status_HS4 AS Status_HS4,
+    in2.C_STRING AS C_STRING
   
   FROM Reformat_1 AS in0
   INNER JOIN goods_classification AS in1
      ON in0.SERVICE_LABEL != in1.Status_HS4
+  INNER JOIN ALL_TYPE_TABLE_NON_GEOMETRY AS in2
+     ON in0.COUNTRY_CODE != in2.C_STRING
 
 )
 
