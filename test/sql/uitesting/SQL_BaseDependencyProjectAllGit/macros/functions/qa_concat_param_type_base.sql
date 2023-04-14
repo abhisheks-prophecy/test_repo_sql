12b8d20119a13bb74316f6d13d615190a1242f6c
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-04-14 06:50:14 +0000

    ❇️ New     - Function        - qa_concat_macro_base_column        - qa_concat_macro_base        - qa_macro_call_another_macro_base_column
    🔁 Modified
        - Model
           - model_with_only_seed
        - Function
           - qa_macro_call_another_macro_base
           - _qa_concat_param_type_base

--- test/sql/uitesting/SQL_BaseDependencyProjectAllGit/macros/functions/qa_concat_param_type_base.sql
+++ test/sql/uitesting/SQL_BaseDependencyProjectAllGit/macros/functions/qa_concat_param_type_base.sql
@@ -1,5 +1,5 @@
-{% macro _qa_concat_param_type_base(input_string='hello', int_param=10) %}
-concat({{input_string}}, {{int_param}})
+{% macro _qa_concat_param_type_base(input_string='hello', int_param_or_col=10) %}
+concat('{{input_string}}', {{int_param}})
 {% endmacro %}
 
  {% macro multi_macro_expressions_base(param_float=12, param_array=[1, 2, 3], param_dict=[1, 2, 3]) %}
@@ -14,6 +14,18 @@ ROUND({{n1}}, {{scale}})
 ROUND({{n1}}, {{scale}})
 {% endmacro %}
 
- {% macro qa_macro_call_another_macro_base(final_param='random data') %}
-concat({{ qa_concat_macro(final_param) }}, {{final_param}})
+ {% macro qa_concat_macro_base(param1_string_only='hello') %}
+concat('{{param1_string_only}}', 'hellomain')
+{% endmacro %}
+
+ {% macro qa_macro_call_another_macro_base(final_param_string_only='random data') %}
+concat({{ qa_concat_macro_base(final_param_string_only) }}, '{{final_param_string_only}}')
+{% endmacro %}
+
+ {% macro qa_concat_macro_base_column(param1_column) %}
+concat({{param1_column}}, 'hellomain')
+{% endmacro %}
+
+ {% macro qa_macro_call_another_macro_base_column(param_column) %}
+concat({{ qa_concat_macro_base_column(param_column) }}, {{param_column}})
 {% endmacro %}
