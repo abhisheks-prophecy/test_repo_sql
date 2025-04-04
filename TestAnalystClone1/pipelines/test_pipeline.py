ebd02e900bbf529c5918e75ba5459dc080ad8cc9
abhisheks@prophecy.io <abhisheks@prophecy.io>
2025-04-04 15:13:13 +0000

    Cloning prophecy project main branch

---
+++ TestAnalystClone1/pipelines/test_pipeline.py
@@ -0,0 +1,11 @@
+Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
+
+with DAG(Schedule = Schedule):
+    S3Source_0 = SourceTask(
+        task_id = "S3Source_0", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3", id = "s3"), 
+        format = CSVFormat(header = True, separator = ","), 
+        filePath = "/annual-enterprise-survey-2020-financial-year-provisional-csv.csv"
+    )
